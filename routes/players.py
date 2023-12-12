0b58cfc2ad3742524a45c149f0254c8e6cc10829
Liden <lninyow@gmail.com>
2023-12-12 23:42:55 +0800

    updated player routes

--- routes/players.py
+++ routes/players.py
@@ -1,3 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint,jsonify,request
+from db.players import create_player, get_player, get_players, update_player, delete_player
 
 players = Blueprint("players", __name__)
+
+@players.get("/")
+def handle_get_all_players():
+    data = get_players()
+    return jsonify(data)
+
+@players.get("/<int:id>")
+def handle_get_player(id):
+    data = get_player(id)
+    return jsonify(data)
+
+@players.post("/")
+def handle_create_player():
+    body = request.json
+    name = body["name"]
+    total_wins = body["totalWins"]
+    player_id = create_player(name, total_wins)
+    return jsonify({"player_id": player_id})
+
+@players.put("/<int:id>")
+def handle_update_player(id):
+    body = request.json
+    name = body["name"]
+    total_wins = body["totalWins"]
+    player_id = update_player(id, name, total_wins)
+    return jsonify({"player_id": player_id})
+
+@players.delete("/<int:id>")
+def handle_delete_player(id):
+    player_id = delete_player(id)
+    return jsonify({"player_id": player_id})
