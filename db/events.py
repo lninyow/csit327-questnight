5f5a862843b2731a71386ce8678b1ee1d1666ee5
Nathaniel Tampus <nate@mugnavo.com>
2023-12-12 17:57:30 +0800

    move db-related stuff to db folder

--- db/events.py
+++ db/events.py
@@ -1,4 +1,5 @@
-from database import fetchone, fetchall
+from db.database import fetchone, fetchall
+
 
 def create_event(start_date, end_date, game_id, winner_id):
     query = "INSERT INTO events (start_date, end_date, game_id, winner_id) VALUES (%s, %s, %s, %s)"
@@ -6,10 +7,12 @@ def create_event(start_date, end_date, game_id, winner_id):
     result = fetchone(query, params)
     return result["event_id"]
 
+
 def get_events():
     query = "SELECT * FROM events"
     result = fetchall(query)
     return result
+
 
 def get_event(event_id):
     query = "SELECT * FROM events WHERE event_id = %s"
@@ -17,11 +20,13 @@ def get_event(event_id):
     result = fetchone(query, params)
     return result
 
+
 def update_event(event_id, start_date, end_date, game_id, winner_id):
     query = "UPDATE events SET start_date = %s, end_date = %s, game_id = %s, winner_id = %s WHERE event_id = %s"
     params = (start_date, end_date, game_id, winner_id, event_id)
     result = fetchone(query, params)
     return result["event_id"]
+
 
 def delete_event(event_id):
     query = "DELETE FROM events WHERE event_id = %s"
--- db/games.py
+++ db/games.py
@@ -1,4 +1,5 @@
-from database import fetchone, fetchall
+from db.database import fetchone, fetchall
+
 
 def create_game(title, cover_url, description):
     query = "INSERT INTO games (title, coverUrl, description) VALUES (%s, %s, %s)"
@@ -6,10 +7,12 @@ def create_game(title, cover_url, description):
     result = fetchone(query, params)
     return result["game_id"]
 
+
 def get_games():
     query = "SELECT * FROM games"
     result = fetchall(query)
     return result
+
 
 def get_game(game_id):
     query = "SELECT * FROM games WHERE game_id = %s"
@@ -17,11 +20,13 @@ def get_game(game_id):
     result = fetchone(query, params)
     return result
 
+
 def update_game(game_id, title, cover_url, description):
     query = "UPDATE games SET title = %s, coverUrl = %s, description = %s WHERE game_id = %s"
     params = (title, cover_url, description, game_id)
     result = fetchone(query, params)
     return result["game_id"]
+
 
 def delete_game(game_id):
     query = "DELETE FROM games WHERE game_id = %s"
